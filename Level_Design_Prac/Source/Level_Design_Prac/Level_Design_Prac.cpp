679f2db67365e91ce6d3bb9855c38367c9365bf2
Yousquid <ly2456@nyu.edu>
2025-03-12 09:29:19 +0800

    patroling enemy added

---
+++ Level_Design_Prac/Source/Level_Design_Prac/Level_Design_Prac.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Level_Design_Prac.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Level_Design_Prac, "Level_Design_Prac" );
