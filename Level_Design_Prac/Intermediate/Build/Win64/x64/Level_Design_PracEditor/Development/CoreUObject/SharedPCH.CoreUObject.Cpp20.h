6ded9dde7cb2709a7042d27e2b9a34b48afd7375
Yousquid <ly2456@nyu.edu>
2025-04-07 10:48:29 -0400

    CHANGES I DONT KNOW BUT GOOD

--- Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++ Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -1,3 +1,7 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+<<<<<<< Updated upstream
 #include "C:/Users/30791/Documents/GitHub/Level-Design-Level/Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+=======
+#include "C:/Users/30791/Level-Design-Level/Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+>>>>>>> Stashed changes
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
--- Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
+++ Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
@@ -1,3 +1,7 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
+<<<<<<< Updated upstream
 #include "C:/Users/30791/Documents/GitHub/Level-Design-Level/Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
+=======
+#include "C:/Users/30791/Level-Design-Level/Level_Design_Prac/Intermediate/Build/Win64/x64/Level_Design_PracEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
+>>>>>>> Stashed changes
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
